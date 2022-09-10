97d78c0d1ec8303ab30f2d0270a57ae8bc2b5786
Konstantin Bondariev <bondariev.konstantin@gmail.com>
2022-09-10 18:51:09 +0300

    nav-bar__linl js

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,6 +7,11 @@ class Header {
         headerLogogClass: '.header__logo',
         headerStyleClass: '.header__style',
         nextElemClass: '.main',
+        navBarShopId: '#linkShop',
+        navBarLernId: '#linkLearn',
+        navBarOpenShopId: '#shop',
+        navBarOpenLernId: '#lern',
+        btnClouseId: '.open-btn',
       },
       ...p,
     };
@@ -15,15 +20,34 @@ class Header {
     this.headerLogo = this.headerMenu.querySelector(settings.headerLogogClass);
     this.headerStyle = this.headerMenu.querySelector(settings.headerStyleClass);
     this.nextElem = document.querySelector(settings.nextElemClass);
+    this.linkShop = this.header.querySelector(settings.navBarShopId);
+    this.linkLearn = this.header.querySelector(settings.navBarLernId);
+    this.openShop = this.header.querySelector(settings.navBarOpenShopId);
+    this.openLern = this.header.querySelector(settings.navBarOpenLernId);
+    this.btnClouse = this.header.querySelectorAll(settings.btnClouseId);
   }
 
-  pageWatch() {
+  _openLink(e) {
+    this.target = e.target;
+
+    this.target == this.linkShop
+      ? this.openShop.classList.add('open-link')
+      : this.target == this.linkLearn
+      ? this.openLern.classList.add('open-link')
+      : null;
+  }
+  _clouseLink() {
+    this.openShop.classList.remove('open-link');
+    this.openLern.classList.remove('open-link');
+  }
+
+  _pageWatch() {
     let y = this.nextElem.getBoundingClientRect();
     if (y.y) return true;
   }
 
-  changeHeaderMenu() {
-    if (this.pageWatch()) {
+  _changeHeaderMenu() {
+    if (this._pageWatch()) {
       this.header.classList.add('header--not-top');
       this.headerLogo.classList.add('header__logo--non-top');
       this.headerStyle.classList.add('header__style--non-top');
@@ -33,11 +57,18 @@ class Header {
       this.headerStyle.classList.remove('header__style--non-top');
     }
   }
+  _initListeners() {
+    this.headerMenu.addEventListener('click', this._openLink.bind(this));
+    this.btnClouse[0].addEventListener('click', () => this._clouseLink());
+    this.btnClouse[1].addEventListener('click', () => this._clouseLink());
+  }
 
   init() {
+    console.log(this.linkLearn);
+    this._initListeners();
     this.watch = setInterval(() => {
-      this.pageWatch();
-      this.changeHeaderMenu();
+      this._pageWatch();
+      this._changeHeaderMenu();
     }, 50);
   }
 }
